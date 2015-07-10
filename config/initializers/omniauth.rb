44db870aa64fa8e3d11d333655588cc1bf563337
Max Tran <max.thien.tran@gmail.com>
2015-07-09 22:47:09 -0700

    omniauth done finally

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
 
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-           scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+  provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"],
+           scope: 'profile', image_aspect_ratio: 'circle', image_size: 48, access_type: 'online', name: 'google'
 
 
   OmniAuth.config.on_failure = Proc.new do |env|
